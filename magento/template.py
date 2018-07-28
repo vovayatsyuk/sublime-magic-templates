291a829e76881ebb5408de4ef4f9157e61dbcb1f
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-07-28 19:09:36 +0300

    Dict is not ordered, use list instead

--- magento/template.py
+++ magento/template.py
@@ -22,27 +22,24 @@ class Template:
         return content.format(**Variables(self.filePath).extract(variables))
 
     def __match(self):
-        template = None
-
         if self.filePath is None:
-            return template
+            return None
 
-        rules = {
-            'composer\.json': 'composer.json.txt',
-            # 'Model/.*Repository\.php': 'Model/EntityRepository.php',
-            # 'Model/.*/Source/.*': 'Model/Config/Source.php',
-            # 'Model/.*/DataProvider\.php': 'Model/Source/Options.php',
-            # 'Model/ResourceModel/*/Grid/Collection.php': 'Model/Source/Options.php',
-            # 'Model/ResourceModel/*/Collection.php': 'Model/Source/Options.php',
-            # 'Model/ResourceModel/.*\.php': 'Model/ResourceModel/Entity.php',
-            # 'Model/.*\.php': 'Model/Entity.php',
-            '.*\.php': 'default.php.txt'
-        }
+        rules = [
+            ('composer\.json', 'composer.json.txt'),
+            # ('Model/.*Repository\.php', 'Model/EntityRepository.php'),
+            # ('Model/.*/Source/.*', 'Model/Config/Source.php'),
+            # ('Model/.*/DataProvider\.php', 'Model/Source/Options.php'),
+            # ('Model/ResourceModel/*/Grid/Collection.php', 'Model/Source/Options.php'),
+            # ('Model/ResourceModel/*/Collection.php', 'Model/Source/Options.php'),
+            # ('Model/ResourceModel/.*\.php', 'Model/ResourceModel/Entity.php'),
+            # ('Model/.*\.php', 'Model/Entity.php'),
+            ('.*\.php', 'default.php.txt')
+        ]
 
-        for pattern in rules:
+        for pattern, path in rules:
             r = re.compile(pattern)
             if r.search(self.filePath) is not None:
-                template = rules[pattern]
-                break
+                return path
 
-        return template
+        return None
