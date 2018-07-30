f09fa7f14927787b387ab855f4a98e8dedb06743
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-07-30 23:05:59 +0300

    Code refactoring

---
+++ magento/app.py
@@ -0,0 +1,6 @@
+class App:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def get_type(self):
+        return 'magento2'
--- magento/template.py
+++ magento/template.py
@@ -1,22 +1,28 @@
 import sublime
+import json
 import re
+import os
 
 from string import Formatter
+from collections import OrderedDict
+from .app import App
 from .placeholders import Placeholders
 
 class Template:
     def __init__(self, file_path):
         self.file_path = file_path
-
-    def _get_base_path(self):
-        return 'Packages/sublime-magento/magento/templates/m2/'
+        self.app = App(file_path)
 
     def render(self):
         template = self._match()
         if template is None:
             return '';
 
-        content = sublime.load_resource(self._get_base_path() + template)
+        content = sublime.load_resource(os.sep.join([
+            'Packages/sublime-magento/magento/templates',
+            self.app.get_type(),
+            template
+        ]))
         placeholders = [keys[1] for keys in Formatter().parse(content) if keys[1] is not None]
 
         return content.format(**Placeholders(self.file_path).extract(placeholders))
@@ -25,22 +31,16 @@ class Template:
         if self.file_path is None:
             return None
 
-        rules = [
-            ('composer\.json', 'composer.json.txt'),
-            # ('Model/.*Repository\.php', 'Model/EntityRepository.php'),
-            # ('Model/.*/Source/.*', 'Model/Config/Source.php'),
-            # ('Model/.*/DataProvider\.php', 'Model/Source/Options.php'),
-            # ('Model/ResourceModel/*/Grid/Collection.php', 'Model/Source/Options.php'),
-            # ('Model/ResourceModel/*/Collection.php', 'Model/Source/Options.php'),
-            # ('Model/ResourceModel/.*\.php', 'Model/ResourceModel/Entity.php'),
-            # ('Model/.*\.php', 'Model/Entity.php'),
-            ('Helper/.*\.php', 'Helper/Data.php.txt'),
-            ('.*\.php', 'default.php.txt')
-        ]
+        content = sublime.load_resource(os.sep.join([
+            'Packages/sublime-magento/magento/templates',
+            self.app.get_type(),
+            'rules.json'
+        ]))
+        rules = json.loads(content, object_pairs_hook = OrderedDict)
 
-        for pattern, path in rules:
+        for pattern in rules:
             r = re.compile(pattern)
             if r.search(self.file_path) is not None:
-                return path
+                return rules[pattern]
 
         return None
