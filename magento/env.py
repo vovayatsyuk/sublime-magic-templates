809c9cdc9ea6603d4597454e654463949f8d2f38
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-07-31 21:01:28 +0300

    Basic logic in Env class

--- magento/env.py
+++ magento/env.py
@@ -1,6 +1,17 @@
+from .composer import Composer
+
 class Env:
     def __init__(self, file_path):
         self.file_path = file_path
+        self.composer = Composer(file_path)
 
     def get_app(self):
-        return 'magento2'
+        app = self.composer.get_type()
+        stopwords = {
+            'magento2-': 'magento2',
+            'magento-': 'magento1'
+        }
+        for key in stopwords:
+            if key in app:
+                return stopwords[key]
+        return None
