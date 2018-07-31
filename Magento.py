ee9f65f0ad590b99aa51fd3f7e00cdb390d00e1a
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-07-31 21:04:42 +0300

    Added comment about wrong behaviour when opening empty file

--- Magento.py
+++ Magento.py
@@ -61,6 +61,8 @@ class InsertNamespaceCommand(sublime_plugin.TextCommand):
 
 class GenerateContentOnFileCreation(sublime_plugin.EventListener):
     def on_load(self, view):
+        # @todo: proper check for newly created file.
+        # current logic returns true for opened empty file
         if view.file_name() is not None and view.size() == 0:
             view.run_command('insert_snippet', {
                 'contents': Template(view.file_name()).render()
