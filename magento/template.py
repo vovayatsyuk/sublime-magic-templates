8deb1a8ffacc3639c2439a267ffdaeaba88dcb73
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-07-31 21:01:59 +0300

    Added ability to pass template to render as method argument

--- magento/template.py
+++ magento/template.py
@@ -10,37 +10,35 @@ from .placeholders import Placeholders
 
 class Template:
     def __init__(self, file_path):
+        self.base_dir = 'Packages/sublime-magento/magento/templates'
         self.file_path = file_path
         self.env = Env(file_path)
 
-    def render(self):
-        template = self._match()
-        if template is None:
-            return '';
+    def render(self, template_path=None):
+        if template_path is None:
+            template_path = self.guess_template_path()
 
-        content = sublime.load_resource(os.sep.join([
-            'Packages/sublime-magento/magento/templates',
-            self.env.get_app(),
-            template
-        ]))
+        content = sublime.load_resource(os.sep.join([self.base_dir, template_path]))
         placeholders = [keys[1] for keys in Formatter().parse(content) if keys[1] is not None]
 
         return content.format(**Placeholders(self.file_path).extract(placeholders))
 
-    def _match(self):
-        if self.file_path is None:
+    def guess_template_path(self):
+        app = self.env.get_app()
+        if app is None:
             return None
 
-        content = sublime.load_resource(os.sep.join([
-            'Packages/sublime-magento/magento/templates',
-            self.env.get_app(),
-            'rules.json'
-        ]))
+        path = None
+        content = sublime.load_resource(os.sep.join([self.base_dir, app, 'rules.json']))
         rules = json.loads(content, object_pairs_hook = OrderedDict)
 
         for pattern in rules:
             r = re.compile(pattern)
             if r.search(self.file_path) is not None:
-                return rules[pattern]
+                path = rules[pattern]
+                break
 
-        return None
+        if path is not None:
+            path = app + os.sep + path
+
+        return path
