bec04a09be5cde2348b23d857f4b12539779e4e2
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-08-01 23:19:56 +0300

    Added todo about improved app name detection

--- magento/env.py
+++ magento/env.py
@@ -7,13 +7,17 @@ class Env:
 
     def get_app(self):
         app = self.composer.get_type()
-        stopwords = {
-            'magento2-': 'magento2',
-            'magento-': 'magento1'
-        }
-        for key in stopwords:
-            if key in app:
-                return stopwords[key]
+        if app is not None:
+            knowntypes = {
+                'magento2-': 'magento2',
+                'magento-': 'magento1'
+            }
+            for key in knowntypes:
+                if key in app:
+                    return knowntypes[key]
+
+        # @todo: try to detect by `require` section
 
         # @todo: fallback to default value from config
+
         return None
