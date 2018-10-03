49e8373865d29c1c708280048f5132c5e32bf63c
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2018-10-03 22:08:25 +0300

    Code cleanup

--- MagicTemplates.py
+++ MagicTemplates.py
@@ -2,45 +2,32 @@ import sublime_plugin
 
 from .mt.app import App
 
+def insert_snippet(view, contents):
+    if contents is None:
+        return;
+    view.run_command('insert_snippet', {
+        'contents': contents
+    })
+
 class GenerateContentCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        contents = App(self.view.file_name()).render_template()
-        if contents is not None:
-            self.view.run_command('insert_snippet', {
-                'contents': contents
-            })
+        insert_snippet(self.view, App(self.view.file_name()).render_template())
 
 class GenerateContentOnFileCreation(sublime_plugin.EventListener):
     def on_load(self, view):
         # @todo: proper check for newly created file.
         # current logic returns true for opened empty file
         if view.file_name() is not None and view.size() == 0:
-            contents = App(view.file_name()).render_template()
-            if contents is not None:
-                view.run_command('insert_snippet', {
-                    'contents': contents
-                })
+            insert_snippet(view, App(view.file_name()).render_template())
 
 class InsertClassNameCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        contents = App(self.view.file_name()).render_snippet('classname')
-        if contents is not None:
-            self.view.run_command('insert_snippet', {
-                'contents': contents
-            })
+        insert_snippet(self.view, App(self.view.file_name()).render_snippet('classname'))
 
 class InsertNamespaceCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        contents = App(self.view.file_name()).render_snippet('namespace')
-        if contents is not None:
-            self.view.run_command('insert_snippet', {
-                'contents': contents
-            })
+        insert_snippet(self.view, App(self.view.file_name()).render_snippet('namespace'))
 
 class InsertIfIpCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        contents = App(self.view.file_name()).render_snippet('ifip')
-        if contents is not None:
-            self.view.run_command('insert_snippet', {
-                'contents': contents
-            })
+        insert_snippet(self.view, App(self.view.file_name()).render_snippet('ifip'))
